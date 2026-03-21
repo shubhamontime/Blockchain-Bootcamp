e238147f3f76b47432078bd6b4f39af4811f5ee7
SHUBHAM <shubhamkumar29141@gmail.com>
2026-03-21 17:21:50 +0530

    Update msg-sender.sol

--- msg-sender.sol
+++ msg-sender.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Shubham {
+
+    address public owner;
+
+    // Constructor sets owner at deployment
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Constructor and state initialization both do same work
+    // Owner does NOT change when account changes
+    // Only msg.sender changes when different account calls a function
+
+    function calledByMsgSender() public view returns(address){
+        return msg.sender;
+    }
+}
